e574efaa815034a962f38f93d615de9b9d759470
Kyle Harris <kyle.harris@amergint.com>
2020-12-17 12:09:44 -0700

    Cleaned up 17 a bit more using HashSets instead of HashMaps

--- src/day17.rs
+++ src/day17.rs
@@ -1,24 +1,12 @@
-use std::collections::{HashMap, HashSet};
-
-#[derive(Copy, Clone, PartialEq, Debug)]
-pub enum State {
-    Active,
-    Inactive,
-}
+use std::collections::HashSet;
 
 #[aoc_generator(day17)]
-pub fn load_input(input: &str) -> HashMap<(i64, i64, i64), State> {
-    let mut output: HashMap<(i64, i64, i64), State> = HashMap::new();
+pub fn load_input(input: &str) -> HashSet<(i64, i64, i64)> {
+    let mut output: HashSet<(i64, i64, i64)> = HashSet::new();
     for (x, line) in input.lines().enumerate() {
         for (y, c) in line.chars().enumerate() {
-            match c {
-                '#' => {
-                    output.insert((x as i64, y as i64, 0), State::Active);
-                }
-                '.' => {
-                    output.insert((x as i64, y as i64, 0), State::Inactive);
-                }
-                _ => (),
+            if let '#' = c {
+                output.insert((x as i64, y as i64, 0));
             }
         }
     }
@@ -44,78 +32,55 @@ pub fn get_neighbors(pos: &(i64, i64, i64)) -> Vec<(i64, i64, i64)> {
     output
 }
 
-pub fn count_active_neighbors(key: &(i64, i64, i64), map: &HashMap<(i64, i64, i64), State>) -> i64 {
+pub fn count_active_neighbors(key: &(i64, i64, i64), map: &HashSet<(i64, i64, i64)>) -> i64 {
     let mut active_cntr = 0;
     for loc in get_neighbors(key) {
-        if let Some(v) = map.get(&loc) {
-            if let State::Active = v {
-                active_cntr += 1;
-            }
+        if map.get(&loc).is_some() {
+            active_cntr += 1;
         }
     }
     active_cntr
 }
 
-pub fn step(map: &HashMap<(i64, i64, i64), State>) -> HashMap<(i64, i64, i64), State> {
+pub fn step(set: &HashSet<(i64, i64, i64)>) -> HashSet<(i64, i64, i64)> {
     let mut candidates: HashSet<(i64, i64, i64)> = HashSet::new();
-    for (k, _v) in map.iter().filter(|(_k, &v)| v == State::Active) {
+    for k in set.iter() {
         candidates.insert(*k);
         for loc in get_neighbors(k) {
             candidates.insert(loc);
         }
     }
 
-    let mut new_map: HashMap<(i64, i64, i64), State> = HashMap::new();
+    let mut new_set: HashSet<(i64, i64, i64)> = HashSet::new();
     for loc in candidates {
         // For each candidate, count its active neighbors
-        let active_cntr = count_active_neighbors(&loc, map);
+        let active_cntr = count_active_neighbors(&loc, set);
 
-        // Add entry to new map
-        if let Some(v) = map.get(&loc) {
-            match v {
-                State::Active => {
-                    if active_cntr == 2 || active_cntr == 3 {
-                        new_map.insert(loc, State::Active);
-                    } else {
-                        new_map.insert(loc, State::Inactive);
-                    }
-                }
-                State::Inactive => {
-                    if active_cntr == 3 {
-                        new_map.insert(loc, State::Active);
-                    } else {
-                        new_map.insert(loc, State::Inactive);
-                    }
-                }
+        // Add entry to new set
+        if set.get(&loc).is_some() {
+            // If currently Active...
+            if active_cntr == 2 || active_cntr == 3 {
+                new_set.insert(loc);
             }
         } else {
-            // Not yet in map then it's Inactive, so apply appropriate logic
+            // If currently Inactive...
             if active_cntr == 3 {
-                new_map.insert(loc, State::Active);
-            } else {
-                new_map.insert(loc, State::Inactive);
+                new_set.insert(loc);
             }
         }
     }
-    new_map
+    new_set
 }
 
 #[aoc(day17, part1)]
-pub fn part1(input: &HashMap<(i64, i64, i64), State>) -> u64 {
-    let mut last_map: HashMap<(i64, i64, i64), State> = input.clone();
+pub fn part1(input: &HashSet<(i64, i64, i64)>) -> usize {
+    let mut last_set: HashSet<(i64, i64, i64)> = input.clone();
     for _ in 0..6 {
-        let new_map = step(&last_map);
-        last_map = new_map;
+        let new_set = step(&last_set);
+        last_set = new_set;
     }
 
-    let mut cntr = 0;
-    for state in last_map.values() {
-        if *state == State::Active {
-            cntr += 1;
-        }
-    }
-
-    cntr
+    last_set.len()
 }
 
 pub fn get_neighbors2(pos: &(i64, i64, i64, i64)) -> Vec<(i64, i64, i64, i64)> {
@@ -145,83 +110,60 @@ pub fn get_neighbors2(pos: &(i64, i64, i64, i64)) -> Vec<(i64, i64, i64, i64)> {
 
 pub fn count_active_neighbors2(
     key: &(i64, i64, i64, i64),
-    map: &HashMap<(i64, i64, i64, i64), State>,
+    set: &HashSet<(i64, i64, i64, i64)>,
 ) -> i64 {
     let mut active_cntr = 0;
     for loc in get_neighbors2(key) {
-        if let Some(v) = map.get(&loc) {
-            if let State::Active = v {
-                active_cntr += 1;
-            }
+        if set.get(&loc).is_some() {
+            active_cntr += 1;
         }
     }
     active_cntr
 }
 
-pub fn step2(map: &HashMap<(i64, i64, i64, i64), State>) -> HashMap<(i64, i64, i64, i64), State> {
+pub fn step2(set: &HashSet<(i64, i64, i64, i64)>) -> HashSet<(i64, i64, i64, i64)> {
     let mut candidates: HashSet<(i64, i64, i64, i64)> = HashSet::new();
-    for (k, _v) in map.iter().filter(|(_k, &v)| v == State::Active) {
+    for k in set.iter() {
         candidates.insert(*k);
         for loc in get_neighbors2(k) {
             candidates.insert(loc);
         }
     }
 
-    let mut new_map: HashMap<(i64, i64, i64, i64), State> = HashMap::new();
+    let mut new_set: HashSet<(i64, i64, i64, i64)> = HashSet::new();
     for loc in candidates {
         // For each candidate, count its active neighbors
-        let active_cntr = count_active_neighbors2(&loc, map);
+        let active_cntr = count_active_neighbors2(&loc, set);
 
-        // Add entry to new map
-        if let Some(v) = map.get(&loc) {
-            match v {
-                State::Active => {
-                    if active_cntr == 2 || active_cntr == 3 {
-                        new_map.insert(loc, State::Active);
-                    } else {
-                        new_map.insert(loc, State::Inactive);
-                    }
-                }
-                State::Inactive => {
-                    if active_cntr == 3 {
-                        new_map.insert(loc, State::Active);
-                    } else {
-                        new_map.insert(loc, State::Inactive);
-                    }
-                }
+        // Add entry to new set
+        if set.get(&loc).is_some() {
+            // If currently Active...
+            if active_cntr == 2 || active_cntr == 3 {
+                new_set.insert(loc);
             }
         } else {
-            // Not yet in map then it's Inactive, so apply appropriate logic
+            // If currently Inactive...
             if active_cntr == 3 {
-                new_map.insert(loc, State::Active);
-            } else {
-                new_map.insert(loc, State::Inactive);
+                new_set.insert(loc);
             }
         }
     }
-    new_map
+    new_set
 }
 
 #[aoc(day17, part2)]
-pub fn part2(input: &HashMap<(i64, i64, i64), State>) -> u64 {
-    let mut last_map: HashMap<(i64, i64, i64, i64), State> = HashMap::new();
-    for (k, v) in input {
-        last_map.insert((k.0, k.1, k.2, 0), *v);
+pub fn part2(input: &HashSet<(i64, i64, i64)>) -> usize {
+    let mut last_set: HashSet<(i64, i64, i64, i64)> = HashSet::new();
+    for loc in input {
+        last_set.insert((loc.0, loc.1, loc.2, 0));
     }
 
     for _ in 0..6 {
-        let new_map = step2(&last_map);
-        last_map = new_map;
+        let new_set = step2(&last_set);
+        last_set = new_set;
     }
 
-    let mut cntr = 0;
-    for state in last_map.values() {
-        if *state == State::Active {
-            cntr += 1;
-        }
-    }
-
-    cntr
+    last_set.len()
 }
 
 #[cfg(test)]
