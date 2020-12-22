dd5f2f571977f3145f07e18fe657abfda52e750f
Kyle Harris <kyle.harris@amergint.com>
2020-12-21 17:21:02 -0700

    Half baked bad idea for 19 part 2

--- src/day19.rs
+++ src/day19.rs
@@ -1,11 +1,11 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use regex::Regex;
 
 
 #[derive(Debug, Clone)]
 pub struct Node {
     id: usize,
-    used_by: Vec<usize>,
+    used_by: HashSet<usize>,
     value: String,
 }
 
@@ -21,20 +21,31 @@ pub fn load_input(input: &str) -> (HashMap<usize, Node>, Vec<String>) {
         for cap in re.captures_iter(line) {
             let id = cap[1].parse().unwrap();
             let rule_str = cap[2].to_string();
-            let node = Node { id: id, used_by: vec![], value: rule_str.to_string() };
+            let node = Node { id: id, used_by: HashSet::new(), value: rule_str.trim_matches('\"').to_string() };
             rgraph.insert(id, node);
         }
     }
 
     // Now that all our Nodes are present and have associated IDs, lets fill
     // out those reverse edge pointers
-    for (k, v) in rgraph.iter() {
+    let rgraph_clone = rgraph.clone();
+    let renum = Regex::new(r"([0-9]+)").unwrap();
+    for (k, v) in rgraph.iter_mut() {
         // k is an ID for a Node, scan all the Nodes (including this one!) and
         // find all references to ID k.
-        for (k, v) in rgraph.iter() {
-            for cap in renum.captures_iter(v.value) {
+        for (k2, v2) in rgraph_clone.iter() {
+            for cap in renum.captures_iter(&v2.value) {
+
+                // Get k2 node dependencies to scan for k1
+                let mut deps = vec![];
                 for c in cap.iter().skip(1) {
                     deps.push(c.unwrap().as_str().parse().unwrap());
+                }
+
+                // Make the reverse connection if k2 node contains k1 node as a
+                // dependency
+                if deps.contains(k) {
+                    v.used_by.insert(*k2);
                 }
             }
         }
@@ -50,37 +61,55 @@ pub fn load_input(input: &str) -> (HashMap<usize, Node>, Vec<String>) {
 
 pub struct Magic {
     idrepr: Vec<usize>,
+    rgraph: HashMap<usize, Node>,
 }
 
 impl Magic {
 
-    pub fn new() -> Magic {
+    pub fn new(rgraph: HashMap<usize, Node>) -> Magic {
         Magic {
             idrepr: vec![],
+            rgraph,
         }
     }
 
-    pub fn push(&mut self, c: &char) -> Vec<usize> {
-        todo!();
+    pub fn push(&mut self, c: &char) {
+        for (id, node) in self.rgraph.iter() {
+            if node.value == c.to_string() {
+                self.idrepr.push(*id);
+            }
+        }
+    }
+
+    pub fn resolve(&mut self) -> bool {
+        let mut cand = self.idrepr.clone();
+        todo!()
     }
 }
 
 #[aoc(day19, part1)]
 pub fn part1(input: &(HashMap<usize, Node>, Vec<String>)) -> u64 {
 
-    let mut input_clone = input.0.clone();
     let test_vec = &input.1;
 
+    let mut cntr = 0;
     for line in test_vec {
         // Check each candidate
-        let mut magic = Magic::new();
+        let mut magic = Magic::new(input.0.clone());
         for c in line.chars() {
             let idrepr = magic.push(&c);
         }
+
+        println!("test string: {}", line);
+        println!("idrepr: {:?}\n", magic.idrepr);
+
+        if magic.resolve() {
+            // We have a match
+            cntr += 1;
+        }
     }
 
-    let mut cntr = 0;
-    todo!()
+    cntr
 }
 
 #[aoc(day19, part2)]
