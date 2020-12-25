06b44adec8c92ae10c06cf8c8adf40162e8c41ae
Kyle Harris <kaharrs2@gmail.com>
2020-12-24 23:40:16 -0700

    Added 3 and rule, part 1 still works.  Part 2 rule 0 doesn't resolve to a leaf

--- src/day19.rs
+++ src/day19.rs
@@ -1,54 +1,29 @@
+use std::collections::HashMap;
 use regex::Regex;
-use std::collections::{HashMap, HashSet};
+
 
 #[derive(Debug, Clone)]
-pub struct Node {
-    id: usize,
-    used_by: HashSet<usize>,
-    value: String,
+pub enum Rule {
+    Leaf(Vec<String>),
+    Node(String),
 }
 
 #[aoc_generator(day19)]
-pub fn load_input(input: &str) -> (HashMap<usize, Node>, Vec<String>) {
-    let mut rgraph = HashMap::new();
+pub fn load_input(input: &str) -> (HashMap<usize, Rule>, Vec<String>) {
+    let mut rules = HashMap::new();
     let re = Regex::new(r"^([0-9]+): (.+)$").unwrap();
+    let leafre = Regex::new(r#""([ab])""#).unwrap();
 
     let sections: Vec<_> = input.split("\n\n").collect();
 
-    // First create the reverse graph with no edges
     for line in sections[0].lines() {
         for cap in re.captures_iter(line) {
             let id = cap[1].parse().unwrap();
             let rule_str = cap[2].to_string();
-            let node = Node {
-                id: id,
-                used_by: HashSet::new(),
-                value: rule_str.trim_matches('\"').to_string(),
-            };
-            rgraph.insert(id, node);
-        }
-    }
-
-    // Now that all our Nodes are present and have associated IDs, lets fill
-    // out those reverse edge pointers
-    let rgraph_clone = rgraph.clone();
-    let renum = Regex::new(r"([0-9]+)").unwrap();
-    for (k, v) in rgraph.iter_mut() {
-        // k is an ID for a Node, scan all the Nodes (including this one!) and
-        // find all references to ID k.
-        for (k2, v2) in rgraph_clone.iter() {
-            for cap in renum.captures_iter(&v2.value) {
-                // Get k2 node dependencies to scan for k1
-                let mut deps = vec![];
-                for c in cap.iter().skip(1) {
-                    deps.push(c.unwrap().as_str().parse().unwrap());
-                }
-
-                // Make the reverse connection if k2 node contains k1 node as a
-                // dependency
-                if deps.contains(k) {
-                    v.used_by.insert(*k2);
-                }
+            if let Some(m) = leafre.captures(&rule_str) {
+                rules.insert(id, Rule::Leaf(vec![m[1].to_string()]));
+            } else {
+                rules.insert(id, Rule::Node(rule_str));
             }
         }
     }
@@ -58,63 +33,300 @@ pub fn load_input(input: &str) -> (HashMap<usize, Node>, Vec<String>) {
         test_vec.push(line.trim_end().to_string());
     }
 
-    (rgraph, test_vec)
+    (rules, test_vec)
 }
 
-pub struct Magic {
-    idrepr: Vec<usize>,
-    rgraph: HashMap<usize, Node>,
-}
+pub fn resolve_rule(rule: &mut Rule, rulemap: &HashMap<usize, Rule>, max_len: usize) {
 
-impl Magic {
-    pub fn new(rgraph: HashMap<usize, Node>) -> Magic {
-        Magic {
-            idrepr: vec![],
-            rgraph,
-        }
-    }
-
-    pub fn push(&mut self, c: &char) {
-        for (id, node) in self.rgraph.iter() {
-            if node.value == c.to_string() {
-                self.idrepr.push(*id);
+    match rule {
+        Rule::Leaf(value) => (),
+        Rule::Node(value) => {
+            let numsre = Regex::new(r"([0-9]+)").unwrap();
+            let mut deps: Vec<usize> = vec![];
+            for cap in numsre.captures_iter(value) {
+                for c in cap.iter().skip(1) {
+                    deps.push(c.unwrap().as_str().parse().unwrap());
+                }
             }
-        }
-    }
+            //println!("deps: {:?}", deps);
 
-    pub fn resolve(&mut self) -> bool {
-        let mut cand = self.idrepr.clone();
-        todo!()
+            // If we have all deps, resolve them and calc a new Rule::Leaf!...
+            // else just leave it alone for later.
+            for dep in &deps {
+                if let Some(r) = rulemap.get(dep) {
+                    if let Rule::Node(x) = r {
+                        // Bail if we can't resolve yet
+                        return;
+                    }
+                } else {
+                    // Rule not in hashmap, obviously can't resolve
+                    // This should never happen btw
+                    panic!("Requested dependency not in rulemap");
+                }
+            }
+
+            // If we get here then we can resolve the Leaf
+            let leaves = parser(rule, rulemap, max_len);
+
+            *rule = Rule::Leaf(leaves);
+
+            //println!("Resolved rule: {:?}", *rule);
+        },
+    }
+}
+
+pub fn parser(rule: &Rule, rulemap: &HashMap<usize, Rule>, max_len: usize) -> Vec<String> {
+    let mut output: Vec<String> = vec![];
+    match rule {
+        Rule::Node(rule_string) => {
+            let re_single = Regex::new(r"^([0-9]+)$").unwrap();
+
+            //println!("rule_string: {}", rule_string.to_string());
+
+            for cap in re_single.captures_iter(rule_string) {
+                //println!("Single regex tripped: {}", cap[0].to_string());
+                let num: usize = cap[1].parse().unwrap();
+                let rule = rulemap.get(&num).unwrap(); // Safe because we know it's in map
+                if let Rule::Leaf(value_vec) = rule {
+                    for v in value_vec {
+                        output.push(v.to_string());
+                    }
+                } else {
+                    panic!("Should never get here");
+                }
+
+                // Ok to bail here, if we matched it was just this number
+                return output;
+            }
+
+
+            // Handle single or no ands
+            let re_or = Regex::new(r"^([0-9]+) \| ([0-9]+)$").unwrap();
+            for cap in re_or.captures_iter(rule_string) {
+                //println!("Or regex tripped: {}", cap[0].to_string());
+                let num1: usize = cap[1].parse().unwrap();
+                let num2: usize = cap[2].parse().unwrap();
+
+                let rule = rulemap.get(&num1).unwrap(); // Safe because we know it's in map
+                if let Rule::Leaf(value_vec) = rule {
+                    for v in value_vec {
+                        output.push(v.to_string());
+                    }
+                } else {
+                    panic!("Should never get here");
+                }
+
+                let rule = rulemap.get(&num2).unwrap(); // Safe because we know it's in map
+                if let Rule::Leaf(value_vec) = rule {
+                    for v in value_vec {
+                        output.push(v.to_string());
+                    }
+                } else {
+                    panic!("Should never get here");
+                }
+
+                // Ok to bail here, if we matched it was just this or
+                return output;
+            }
+
+            // Resolve ands, return strings
+            let re_and2 = Regex::new(r"([0-9]+) ([0-9]+)").unwrap();
+            let re_and3 = Regex::new(r"([0-9]+) ([0-9]+) ([0-9]+)").unwrap();
+
+            let mut and3 = false;
+            for cap in re_and3.captures_iter(rule_string) {
+                println!("Triggered");
+                and3 = true;
+                let mut nums = vec![];
+                nums.push(cap[1].trim_end().parse().unwrap());
+                nums.push(cap[2].trim_end().parse().unwrap());
+                nums.push(cap[3].trim_end().parse().unwrap());
+
+                let mut part_vec_vec = vec![];
+                for num in nums {
+                    let mut part_vec = vec![];
+                    let rule = rulemap.get(&num).unwrap(); // Safe because we know it's in map
+                    if let Rule::Leaf(value_vec) = rule {
+                        for v in value_vec {
+                            let temp = v.to_string();
+                            if temp.len() > max_len {
+                                continue;
+                            }
+                            part_vec.push(temp);
+                        }
+                    } else {
+                        panic!("Should never get here");
+                    }
+
+                    part_vec_vec.push(part_vec);
+                }
+
+                for part1 in &part_vec_vec[0] {
+                    for part2 in &part_vec_vec[1] {
+                        for part3 in &part_vec_vec[2] {
+                            let mut tt = part1.to_string();
+                            tt.push_str(part2);
+                            tt.push_str(part3);
+                            output.push(tt);
+                        }
+                    }
+                }
+            }
+
+            if !and3 {
+                for cap in re_and2.captures_iter(rule_string) {
+                    let mut nums = vec![];
+                    nums.push(cap[1].trim_end().parse().unwrap());
+                    nums.push(cap[2].trim_end().parse().unwrap());
+
+                    let mut part_vec_vec = vec![];
+                    for num in nums {
+                        let mut part_vec = vec![];
+                        let rule = rulemap.get(&num).unwrap(); // Safe because we know it's in map
+                        if let Rule::Leaf(value_vec) = rule {
+                            for v in value_vec {
+                                part_vec.push(v.to_string());
+                            }
+                        } else {
+                            panic!("Should never get here");
+                        }
+
+                        part_vec_vec.push(part_vec);
+                    }
+
+                    for part1 in &part_vec_vec[0] {
+                        for part2 in &part_vec_vec[1] {
+                            let mut tt = part1.to_string();
+                            tt.push_str(part2);
+                            output.push(tt);
+                        }
+                    }
+
+                }
+            }
+
+            //println!("{:?}", output);
+            return output;
+        },
+        Rule::Leaf(string_vec) => return string_vec.to_vec(),
     }
 }
 
 #[aoc(day19, part1)]
-pub fn part1(input: &(HashMap<usize, Node>, Vec<String>)) -> u64 {
+pub fn part1(input: &(HashMap<usize, Rule>, Vec<String>)) -> u64 {
+
+    let mut input_clone = input.0.clone();
     let test_vec = &input.1;
 
+    // What's the maximum length string we care about?
+    let max_len = test_vec.iter().map(|x| x.len()).max().unwrap();
+
+    for _ in 0..10 {
+        let temp = input_clone.clone();
+        for (k, v) in input_clone.iter_mut() {
+            resolve_rule(v, &temp, max_len);
+        }
+        //println!("Rules: {:?}\n", input_clone);
+    }
+
+    //println!("Rule 0: {:?}\n", input_clone.get(&0).unwrap());
+    let rule0 = input_clone.get(&0).unwrap();
+
     let mut cntr = 0;
-    for line in test_vec {
-        // Check each candidate
-        let mut magic = Magic::new(input.0.clone());
-        for c in line.chars() {
-            let idrepr = magic.push(&c);
+    if let Rule::Leaf(rule0_vec) = rule0 {
+        for test in test_vec {
+            if let Some(_) = rule0_vec.iter().filter(|&value| value == test).next() {
+                cntr += 1;
+            }
         }
-
-        println!("test string: {}", line);
-        println!("idrepr: {:?}\n", magic.idrepr);
-
-        if magic.resolve() {
-            // We have a match
-            cntr += 1;
-        }
+    } else {
+        panic!("Rule 0 didn't resolve to a Rule::Leaf() object");
     }
 
     cntr
 }
 
+pub fn resolve_rule2(rule: &mut Rule, rulemap: &HashMap<usize, Rule>, max_len: usize) {
+
+    match rule {
+        Rule::Leaf(value) => (),
+        Rule::Node(value) => {
+            let numsre = Regex::new(r"([0-9]+)").unwrap();
+            let mut deps: Vec<usize> = vec![];
+            for cap in numsre.captures_iter(value) {
+                for c in cap.iter().skip(1) {
+                    deps.push(c.unwrap().as_str().parse().unwrap());
+                }
+            }
+            //println!("deps: {:?}", deps);
+
+            // If we have all deps, resolve them and calc a new Rule::Leaf!...
+            // else just leave it alone for later.
+            for dep in &deps {
+                if let Some(r) = rulemap.get(dep) {
+                    if let Rule::Node(x) = r {
+                        // Bail if we can't resolve yet
+                        return;
+                    }
+                } else {
+                    // Rule not in hashmap, obviously can't resolve
+                    // This should never happen btw
+                    panic!("Requested dependency not in rulemap");
+                }
+            }
+
+            // If we get here then we can resolve the Leaf
+            let leaves = parser(rule, rulemap, max_len);
+
+            *rule = Rule::Leaf(leaves);
+
+            //println!("Resolved rule: {:?}", *rule);
+        },
+    }
+}
+
 #[aoc(day19, part2)]
-pub fn part2(input: &(HashMap<usize, Node>, Vec<String>)) -> u64 {
-    0
+pub fn part2(input: &(HashMap<usize, Rule>, Vec<String>)) -> u64 {
+
+    let mut input_clone = input.0.clone();
+    let test_vec = &input.1;
+
+    // Modify input as specified
+    if let Some(value) = input_clone.get_mut(&8) {
+        *value = Rule::Node("42 | 42 8".to_string());
+    }
+    if let Some(value) = input_clone.get_mut(&11) {
+        *value = Rule::Node("42 31 | 42 11 31".to_string());
+    }
+
+    // What's the maximum length string we care about?
+    let max_len = test_vec.iter().map(|x| x.len()).max().unwrap();
+
+    println!("max_len: {}", max_len);
+
+    for _ in 0..10 {
+        let temp = input_clone.clone();
+        for (k, v) in input_clone.iter_mut() {
+            resolve_rule2(v, &temp, max_len);
+        }
+        //println!("Rules: {:?}\n", input_clone);
+    }
+
+    //println!("Rule 0: {:?}\n", input_clone.get(&0).unwrap());
+    let rule0 = input_clone.get(&0).unwrap();
+
+    let mut cntr = 0;
+    if let Rule::Leaf(rule0_vec) = rule0 {
+        for test in test_vec {
+            if let Some(_) = rule0_vec.iter().filter(|&value| value == test).next() {
+                cntr += 1;
+            }
+        }
+    } else {
+        panic!("Rule 0 didn't resolve to a Rule::Leaf() object");
+    }
+
+    cntr
 }
 
 #[cfg(test)]
@@ -135,8 +347,8 @@ mod test {
 
     #[test]
     fn test_part2() {
-        let input = read_to_string("input/19b.txt").unwrap();
+        let input = read_to_string("input/19c.txt").unwrap();
         let input = load_input(&input);
-        assert_eq!(part2(&input), 0);
+        assert_eq!(part2(&input), 12);
     }
 }
